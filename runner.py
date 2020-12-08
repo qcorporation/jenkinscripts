5c61aa2710a263929eb88bcf5f755ce0ea572395
Quan Nguyen <qnguyen@arxan.com>
2020-12-08 16:32:09 -0500

    tsss

--- runner.py
+++ runner.py
@@ -3,7 +3,7 @@ import time
 def test_method():
     retry = 10
     while( retry != 0 ):
-        time.sleep(1)
+        time.sleep(5)
         print (f'sleeping counter {retry}')
         retry -= 1
 
